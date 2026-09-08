9bb76b0379949718874022e9ffe373cc598a35ae
s3431331 <s3431331@student.rmit.edu.au>
2026-09-09 09:08:09 +1000

    refactor: subtype map
    Moved to constants.js

--- frontend/src/Components/SemesterUI/SemesterComponent.js
+++ frontend/src/Components/SemesterUI/SemesterComponent.js
@@ -1,27 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./SemesterComponent.css";
-import { API_BASE_URL } from "../../constants";
+import { API_BASE_URL, SUB_TYPE_MAP } from "../../constants";
 
 
-const SUB_TYPE_MAP = {
-  1: "Core",
-  2: "Advanced Computer Science",
-  3: "Cyber Security",
-  4: "Enterprise Systems Development",
-  5: "Artificial Intelligence & Machine Learning",
-  6: "Blockchain Technologies",
-  7: "Cloud Computing",
-  8: "Creative Computing",
-  9: "Cyber Assurance",
-  10: "Data Science",
-  11: "Design & Develop for Apple Platform",
-  12: "Enterprise Systems Development",
-  13: "Bioinformatics",
-  14: "Data Analysis",
-  15: "Digital Innovation",
-  16: "University Elective",
-  17: "Program Course",
-};
+
 
 const SUB_TYPE_NAME_TO_ID = Object.entries(SUB_TYPE_MAP).reduce(
   (acc, [id, name]) => {
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,26 @@
 
 export const API_BASE_URL = "http://localhost:3000";
+
+
+
+
+
+const SUB_TYPE_MAP = {
+  1: "Core",
+  2: "Advanced Computer Science",
+  3: "Cyber Security",
+  4: "Enterprise Systems Development",
+  5: "Artificial Intelligence & Machine Learning",
+  6: "Blockchain Technologies",
+  7: "Cloud Computing",
+  8: "Creative Computing",
+  9: "Cyber Assurance",
+  10: "Data Science",
+  11: "Design & Develop for Apple Platform",
+  12: "Enterprise Systems Development",
+  13: "Bioinformatics",
+  14: "Data Analysis",
+  15: "Digital Innovation",
+  16: "University Elective",
+  17: "Program Course",
+};
