3d9bad33178438ec230648f61e4c15cd403d6e44
s3431331 <s3431331@student.rmit.edu.au>
2026-09-16 20:42:49 +1000

    refactor: dead code

--- frontend/src/Components/SemesterUI/SemesterComponent.js
+++ frontend/src/Components/SemesterUI/SemesterComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./SemesterComponent.css";
 import { API_BASE_URL, CREDITS, SUB_TYPE, SUB_TYPE_MAP, SUB_TYPE_NAME_TO_ID, LOCALS } from "../../constants";
 import { isPrereqMet } from "../../utils/courseCategoriser";
