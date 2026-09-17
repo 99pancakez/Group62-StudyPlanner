3baa35e4beecd513367cfb99d766bfe8407046a0
s3431331 <s3431331@student.rmit.edu.au>
2026-09-17 14:40:26 +1000

    fix: sub-type id type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -8,7 +8,7 @@ export interface Course {
     name: string;
     credit: number;
     sub_type_ids?: number[];
-    selected_sub_type_ids?: number[];
+    selected_sub_type_ids?: number;
     year?: number;
     semesters?: Semester[];
     prereqString?: string;
