47a7b86b3ae456e748bda9c90d1abddad2ea96a6
s3431331 <s3431331@student.rmit.edu.au>
2026-09-17 14:55:27 +1000

    refactor: unused imports

--- frontend/src/Components/SemesterUI/SemesterComponent.jsx
+++ frontend/src/Components/SemesterUI/SemesterComponent.jsx
@@ -1,12 +1,9 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import "./SemesterComponent.css";
-import { API_BASE_URL, CREDITS, SUB_TYPE, SUB_TYPE_MAP, SUB_TYPE_NAME_TO_ID, LOCALS } from "../../constants";
+import { API_BASE_URL, CREDITS, SUB_TYPE, SUB_TYPE_MAP, LOCALS } from "../../constants";
 import { isPrereqMet } from "../../utils/courseCategoriser";
 import logger from "../../log";
 import CourseDropdown from "../common/CourseDropdown";
-
-
-
 
 
 
